4875f8655b5a8d135be003d162e338c45f5596e2
nazmul-islam00 <155834778+nazmul-islam00@users.noreply.github.com>
2024-08-12 17:48:14 +0600

    done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,12 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react/prop-types */
+import React from 'react';
+import Product from './Product'
+
+export default function Products(props) {
+  return (
+    <div className='products'>
+      {props.products.map(product => <Product key={product.id} product={product}/>)}
+    </div>
+  )
+}
